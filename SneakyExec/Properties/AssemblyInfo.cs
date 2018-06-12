ce6a1f14807e7749376ea39a10f47b5491f4f939
HackingThings <laplinker@gmail.com>
2018-06-12 09:21:28 -0700

    update names and cleanup

--- SneakyExec/Properties/AssemblyInfo.cs
+++ SneakyExec/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DownloadAndExec")]
+[assembly: AssemblyTitle("SneakyExec")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DownloadAndExec")]
+[assembly: AssemblyProduct("SneakyExec")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
